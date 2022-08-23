588712aeaba53006c070f4ee8d3cf41539554016
Gabriel Vieira <gabrielvieira1@protonmail.com>
2022-08-22 22:37:16 -0300

    Escrevendo as Próprias Funções

--- forca.c
+++ forca.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
 #include <string.h>
 
+void abertura()
+{
+ printf("/****************/\n");
+ printf("/ Jogo de Forca */\n");
+ printf("/****************/\n\n");
+}
+
+void chuta(char chutes[26], int tentativas)
+{
+ char chute;
+ printf("\n\nQual letra? ");
+ scanf("%c", &chute);
+
+ chutes[tentativas] = chute;
+}
+
 int main()
 {
  char palavrasecreta[20];
  sprintf(palavrasecreta, "MELANCIA");
+
+ abertura();
 
  int acertou = 0;
  int enforcou = 0;
@@ -36,12 +54,8 @@ int main()
     printf("_ ");
    }
   }
-
-  char chute;
-  printf("\n\nQual letra? ");
-  scanf("%c", &chute);
-
-  chutes[tentativas] = chute;
+  
+  chuta(chutes, tentativas);
   tentativas++;
 
  } while (!acertou && !enforcou);
